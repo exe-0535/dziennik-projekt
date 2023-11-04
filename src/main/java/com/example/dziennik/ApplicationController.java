12dacf43b65cdb64d2952cfe3508ac97087ac396
exe <igorwdowiczak@gmail.com>
2023-11-04 14:11:43 +0100

    :anchor: Renamed and deleted some files just for the sake of :sparkles: art :sparkles:

--- src/main/java/com/example/dziennik/ApplicationController.java
+++ src/main/java/com/example/dziennik/ApplicationController.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ApplicationController extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ApplicationController.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 486);
         scene.getStylesheets().add(getClass().getResource("styles/login-style.css").toExternalForm());
 
