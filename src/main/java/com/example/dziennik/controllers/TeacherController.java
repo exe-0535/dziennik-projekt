34359c92929ffb015e61e8288c55db66ed12a415
exe-0535 <igorwdowiczak@gmail.com>
2023-11-14 09:23:20 +0100

    :anchor: Teachers can now log in and have a ChoiceBox to choose a unit in the future

--- src/main/java/com/example/dziennik/controllers/TeacherController.java
+++ src/main/java/com/example/dziennik/controllers/TeacherController.java
@@ -1,4 +1,32 @@
 package com.example.dziennik.controllers;
 
-public class TeacherController {
+import com.example.dziennik.helpers.CurrentUser;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TeacherController implements Initializable {
+    @FXML
+    private Label welcomeLabel;
+    @FXML
+    private Button logoutBtn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        welcomeLabel.setText("Zalogowano jako: " + CurrentUser.getCurrentUser().getEmail());
+
+        logoutBtn.setOnAction(event -> {
+            try {
+                SceneController.getLoginScene(event);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
 }
